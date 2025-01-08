e4270b6723a2687ea4e495f00ee8d157317244f2
Luca Modic <lucanmodic@gmail.com>
2025-01-08 12:38:11 -0300

    Fix .gitignore not ignoring files

--- dist/utils/fileUtils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.saveTicketsToJSON = saveTicketsToJSON;
-const fs = require("fs");
-function saveTicketsToJSON(tickets, file) {
-    fs.writeFileSync(file, JSON.stringify(tickets, null, 2), "utf-8");
-}
--- dist/utils/probabilitiesLoader.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loadProbabilities = loadProbabilities;
-const fs = require("fs");
-function loadProbabilities(file) {
-    const data = fs.readFileSync(file, "utf-8");
-    return JSON.parse(data);
-}
